5c5d5bd9b20db4ec1e34a3c54c9a7a5806fa0648
yongshengdev <yongshengdev@163.com>
2022-08-17 15:55:38 +0800

    hello ML Kit

---
+++ HelloMlKit/app/src/main/java/com/sign/hellomlkit/BarcodeAnalyzer.kt
@@ -0,0 +1,41 @@
+package com.sign.hellomlkit
+
+import android.annotation.SuppressLint
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+
+/**
+ * @author: yongshengdev@163.com
+ * @created on 2022/7/22
+ * @description:
+ */
+class BarcodeAnalyzer(private val barcodeListener: BarcodeListener) : ImageAnalysis.Analyzer {
+    // Get an instance of BarcodeScanner
+    private val scanner = BarcodeScanning.getClient()
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            // Pass image to the scanner and have it do its thing
+            scanner.process(image)
+                .addOnSuccessListener { barcodes ->
+                    // Task completed successfully
+                    for (barcode in barcodes) {
+                        barcodeListener(barcode.rawValue ?: "")
+                    }
+                }
+                .addOnFailureListener {
+                    // You should really do something about Exceptions
+                }
+                .addOnCompleteListener {
+                    // It's important to close the imageProxy
+                    imageProxy.close()
+                }
+        }
+    }
+}
+typealias BarcodeListener = (barcode: String) -> Unit
---
+++ HelloMlKit/app/src/main/java/com/sign/hellomlkit/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.sign.hellomlkit
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicBoolean
+
+class MainActivity : AppCompatActivity() {
+
+    private var processingBarcode = AtomicBoolean(false)
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startCamera()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        processingBarcode.set(false)
+    }
+
+
+    private fun startCamera() {
+        // Create an instance of the ProcessCameraProvider,
+        // which will be used to bind the use cases to a lifecycle owner.
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        // Add a listener to the cameraProviderFuture.
+        // The first argument is a Runnable, which will be where the magic actually happens.
+        // The second argument (way down below) is an Executor that runs on the main thread.
+        cameraProviderFuture.addListener({
+            // Add a ProcessCameraProvider, which binds the lifecycle of your camera to
+            // the LifecycleOwner within the application's life.
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            // Initialize the Preview object, get a surface provider from your PreviewView,
+            // and set it on the preview instance.
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(
+                    fragment_scan_barcode_preview_view.surfaceProvider
+                )
+            }
+
+            // Setup the ImageAnalyzer for the ImageAnalysis use case
+            val imageAnalysis = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, BarcodeAnalyzer { barcode ->
+                        searchBarcode(barcode)
+                    })
+                }
+
+            // Select back camera
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                // Unbind any bound use cases before rebinding
+                cameraProvider.unbindAll()
+                // Bind use cases to lifecycleOwner
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
+            } catch (e: Exception) {
+                Log.e("PreviewUseCase", "Binding failed! :(", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun searchBarcode(barcode: String) {
+        Toast.makeText(this, "result: $barcode", Toast.LENGTH_LONG).show()
+    }
+}
