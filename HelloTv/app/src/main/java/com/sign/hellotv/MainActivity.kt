a0b22ce965f621ee6c06212903e5c57e41c96f0f
yongshengdev <yongshengdev@163.com>
2022-08-18 14:37:03 +0800

    集成vlc播放器

--- HelloTv/app/src/main/java/com/sign/hellotv/MainActivity.kt
+++ HelloTv/app/src/main/java/com/sign/hellotv/MainActivity.kt
@@ -1,20 +1,67 @@
 package com.sign.hellotv
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.sign.hellotv.databinding.ActivityMainBinding
+import org.videolan.libvlc.LibVLC
+import org.videolan.libvlc.Media
+import org.videolan.libvlc.MediaPlayer
 
-/**
- * Loads [MainFragment].
- */
 class MainActivity : FragmentActivity() {
+
+    companion object {
+
+        private const val USE_TEXTURE_VIEW = false
+        private const val ENABLE_SUBTITLES = true
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var mLibVLC: LibVLC? = null
+    private var mMediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_browse_fragment, MainFragment())
-                .commitNow()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initPlayerView()
+    }
+
+    private fun initPlayerView() {
+        val filePath = "https://huawei.dailyyoga.com.cn/fullbodypowerflow1548753401799"
+        mLibVLC = LibVLC(this, ArrayList<String>().apply {
+            add("--no-drop-late-frames")
+            add("--no-skip-frames")
+            add("--rtsp-tcp")
+            add("-vvv")
+        })
+        mMediaPlayer = MediaPlayer(mLibVLC)
+        mMediaPlayer?.attachViews(binding.viewVlcLayout, null, ENABLE_SUBTITLES, USE_TEXTURE_VIEW)
+        try {
+            Media(mLibVLC, Uri.parse(filePath)).apply {
+                setHWDecoderEnabled(true, true);
+                // addOption(":network-caching=150");
+                // addOption(":clock-jitter=0");
+                // addOption(":clock-synchro=0");
+                mMediaPlayer?.media = this
+            }.release()
+            mMediaPlayer?.play()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mMediaPlayer?.stop()
+        mMediaPlayer?.detachViews()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMediaPlayer?.release()
+        mLibVLC?.release()
     }
 }
